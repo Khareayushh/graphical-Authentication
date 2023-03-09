e88f7d681f7e1837f2acd4ad15479751768f916e
shaktimaan00 <0105cs201021@oriental.ac.in>
2023-03-09 17:39:24 +0530

    gfgfgfgfggf

--- secret_phrase/public/js/app.js
+++ secret_phrase/public/js/app.js
@@ -1,13 +1,65 @@
 const form = document.querySelector(".form");
 let email = document.getElementById("email");
 
+function generateSecretPhrase(numWords) {
+  const words = [
+    "áppłé",
+    "ßaŋaŋa",
+    "¢h€rr¥",
+    "Ɖrag๏nfruit",
+    "€lderberry",
+    "ƒig",
+    "g®ap€",
+    "h๏ŋ€¥∂€ω",
+    "kiωi",
+    "ł€m๏ŋ",
+    "måŋg๏",
+    "ŋ€ctar¡ŋ€",
+    "๏raŋg€",
+    "ρ€ach",
+    "qυ¡ŋc€",
+    "ra$ρberry",
+    "strawß€rr¥",
+    "t@ŋg€r¡ŋ€",
+    "υgli fruit",
+    "v@ŋ¡ł£a",
+    "ωat€rm€l๏ŋ",
+    "χ¡gυa",
+    "¥€ll๏ω ωat€rm€l๏ŋ",
+    "zยcch¡ŋ¡",
+  ];
+  // Generate a random salt string
+  //   const salt = crypto.getRandomValues(new Uint8Array(16)).join("");
+
+  // Create an array to store the selected words
+  const selectedWords = [];
+
+  // Loop through the number of words requested and select a random word
+  for (let i = 0; i < numWords; i++) {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    selectedWords.push(words[randomIndex]);
+  }
+
+  // Concatenate the selected words and the salt into the secret phrase
+  //   const secretPhrase = selectedWords.join("-") + "-" + salt;
+  const secretPhrase = selectedWords.join("-");
+
+  return secretPhrase;
+}
+
+const phrase = generateSecretPhrase(4);
+
+let mailer;
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     let formData = {
         email: email.value,
+        phrase: phrase,
     }
 
+    mailer = email.value;
     console.log(formData);
     
     let xhr = new XMLHttpRequest();
@@ -25,4 +77,25 @@ form.addEventListener('submit', (e) => {
 
     xhr.send(JSON.stringify(formData));
 
+    let phrase_checker = document.querySelector(".phrase-checker");
+    let form_section = document.querySelector(".form-section");
+    setTimeout(() => {
+        phrase_checker.style.display = "block";
+        form_section.style.display = "none";
+    }, 3000);
+    
+})
+
+let redirect = document.querySelector(".redirect");
+let secret_phrase = document.querySelector(".secret-phrase");
+
+redirect.addEventListener('click', () => {
+    if(secret_phrase.value == phrase){
+        alert("phrase matched");
+        location.assign('http://localhost:5500/GAS-for-Bank-main/');
+        // location.replace('/GAS-for-Bank-main/index.html')
+    }
+    else{
+        alert("wrong phrase");
+    }
 })
--- secret_phrase/server.js
+++ secret_phrase/server.js
@@ -1,50 +1,50 @@
-function generateSecretPhrase(numWords) {
-  const words = [
-    "áppłé",
-    "ßaŋaŋa",
-    "¢h€rr¥",
-    "Ɖrag๏nfruit",
-    "€lderberry",
-    "ƒig",
-    "g®ap€",
-    "h๏ŋ€¥∂€ω",
-    "kiωi",
-    "ł€m๏ŋ",
-    "måŋg๏",
-    "ŋ€ctar¡ŋ€",
-    "๏raŋg€",
-    "ρ€ach",
-    "qυ¡ŋc€",
-    "ra$ρberry",
-    "strawß€rr¥",
-    "t@ŋg€r¡ŋ€",
-    "υgli fruit",
-    "v@ŋ¡ł£a",
-    "ωat€rm€l๏ŋ",
-    "χ¡gυa",
-    "¥€ll๏ω ωat€rm€l๏ŋ",
-    "zยcch¡ŋ¡",
-  ];
-  // Generate a random salt string
-//   const salt = crypto.getRandomValues(new Uint8Array(16)).join("");
+// function generateSecretPhrase(numWords) {
+//   const words = [
+//     "áppłé",
+//     "ßaŋaŋa",
+//     "¢h€rr¥",
+//     "Ɖrag๏nfruit",
+//     "€lderberry",
+//     "ƒig",
+//     "g®ap€",
+//     "h๏ŋ€¥∂€ω",
+//     "kiωi",
+//     "ł€m๏ŋ",
+//     "måŋg๏",
+//     "ŋ€ctar¡ŋ€",
+//     "๏raŋg€",
+//     "ρ€ach",
+//     "qυ¡ŋc€",
+//     "ra$ρberry",
+//     "strawß€rr¥",
+//     "t@ŋg€r¡ŋ€",
+//     "υgli fruit",
+//     "v@ŋ¡ł£a",
+//     "ωat€rm€l๏ŋ",
+//     "χ¡gυa",
+//     "¥€ll๏ω ωat€rm€l๏ŋ",
+//     "zยcch¡ŋ¡",
+//   ];
+//   // Generate a random salt string
+// //   const salt = crypto.getRandomValues(new Uint8Array(16)).join("");
 
-  // Create an array to store the selected words
-  const selectedWords = [];
+//   // Create an array to store the selected words
+//   const selectedWords = [];
 
-  // Loop through the number of words requested and select a random word
-  for (let i = 0; i < numWords; i++) {
-    const randomIndex = Math.floor(Math.random() * words.length);
-    selectedWords.push(words[randomIndex]);
-  }
+//   // Loop through the number of words requested and select a random word
+//   for (let i = 0; i < numWords; i++) {
+//     const randomIndex = Math.floor(Math.random() * words.length);
+//     selectedWords.push(words[randomIndex]);
+//   }
 
-  // Concatenate the selected words and the salt into the secret phrase
-//   const secretPhrase = selectedWords.join("-") + "-" + salt;
-    const secretPhrase = selectedWords.join("-");
+//   // Concatenate the selected words and the salt into the secret phrase
+// //   const secretPhrase = selectedWords.join("-") + "-" + salt;
+//     const secretPhrase = selectedWords.join("-");
 
-  return secretPhrase;
-}
+//   return secretPhrase;
+// }
 
-const phrase = generateSecretPhrase(4);
+// const phrase = generateSecretPhrase(4);
 
 const express = require("express");
 const app = express();
@@ -60,8 +60,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/contact.html");
 });
 
+let mailer;
+
 app.post("/", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -71,24 +73,28 @@ app.post("/", (req, res) => {
     },
   });
 
-//   console.log(phrase)
+  const phrase = req.body.phrase;
+  console.log(phrase);
 
   const mailoptions = {
     from: "0105cs201021@oriental.ac.in",
     to: req.body.email,
     subject: "regarding pass change",
-    text: `hello ayush your secret phrase for changing your grid Pattern is ${phrase}`,
+    text: phrase,
   };
 
-  console.log(phrase)
+  mailer = req.body.email;
+  // console.log(mailer);
+  // console.log(phrase)
 
   transporter.sendMail(mailoptions, (error, info) => {
     if (error) {
       console.log(error);
       res.send("error");
     } else {
-      console.log("Email sent: " + info.response);
+      // console.log("Email sent: " + info);
       res.send("success");
+      // location.replace('../GAS-for-Bank-main/index.html')
     }
   });
 });
@@ -96,3 +102,5 @@ app.post("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`server running on PORT: ${PORT}`);
 });
+
+// export {mailer};
